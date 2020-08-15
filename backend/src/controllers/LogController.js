d9c8a88aa668d52e43eea8b96eb3428772173492
Shyroe <leo.henrique747@gmail.com>
2020-08-15 17:40:20 -0300

    backend api is complete

--- backend/src/controllers/LogController.js
+++ backend/src/controllers/LogController.js
@@ -7,6 +7,7 @@ module.exports = {
       const data = await knex("log")
         .innerJoin("tech", "log.tech_id", "=", "tech.id")
         .select("*");
+      console.log("log index: ", data);
       res.json(data);
     } catch (err) {
       console.log("error: ", err);
@@ -25,6 +26,46 @@ module.exports = {
       const log = data[0];
       console.log("Log Store: ", log);
       res.json(log);
+    } catch (err) {
+      console.log("error: ", err);
+    }
+  },
+  async update(req, res) {
+    const { description, warn } = req.body;
+    const { log_id } = req.params;
+
+    // const dataLog = await knex("log").where("id", log_id).returning("*");
+    // console.log("dataLog: ", dataLog);
+    // const selectedLog = dataLog[0];
+    // const { tech_id } = selectedLog;
+
+    // console.log("update ids: tech_id and log_id", log_id, tech_id);
+    const newLog = {
+      // id: log_id,
+      description,
+      warn,
+    };
+    try {
+      const data = await knex("log")
+        .where("id", log_id)
+        .update(newLog)
+        .returning("*");
+      const logUpdated = data[0];
+      console.log("Log Updated: ", logUpdated);
+      res.json(logUpdated);
+    } catch (err) {
+      console.log("error: ", err);
+    }
+  },
+  async destroy(req, res) {
+    const { id } = req.params;
+    console.log("destroy id: ", id);
+    try {
+      const data = await knex("log").where("id", id).del();
+      console.log("destroy data: ", data);
+      res.json({
+        message: "Log deleted successfuly",
+      });
     } catch (err) {
       console.log("error: ", err);
     }
--- backend/src/routes/main.js
+++ backend/src/routes/main.js
@@ -1,10 +1,15 @@
 const routes = require("express").Router();
 const TechController = require("../controllers/TechController");
+const LogController = require("../controllers/LogController");
 
 //Names to Controllers
 // index; show; store; update; destroy
 
 routes.get("/techs", TechController.getAllTechs);
 routes.post("/techs", TechController.createTech);
+routes.post("/logs", LogController.store);
+routes.put("/logs/:log_id", LogController.update);
+routes.delete("/logs/:id", LogController.destroy);
+routes.get("/logs", LogController.index);
 
 module.exports = routes;
